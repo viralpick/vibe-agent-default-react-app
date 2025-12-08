0e3696043b27adc5013a7778e6761ebbfb681600
enhans-taeyang <taeyang@enhans.ai>
2025-12-08 17:05:29 +0900

    refactor: adjust new style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,8 +8,8 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-[36px] w-full min-w-0 rounded-medium border bg-transparent px-12 py-4 text-label-l shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-label-l file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-label-l",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "file:text-foreground placeholder:text-text-secondary selection:bg-primary selection:text-primary-foreground border-input h-[36px] w-full min-w-0 rounded-medium border bg-gray-0 px-12 py-4 text-label-l shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-label-l file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "focus-visible:border-ring focus-visible:ring-ring/40 focus-visible:ring-[2px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
